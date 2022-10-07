009356ff5b98de6e14ecc3c6643cc93359f30525
Hari Neeraja Siripalli <neeraja.siripalli@ivycomptech.com>
2022-10-07 11:15:09 +0000

    Initial commit

---
+++ react-frontend/organicfruitshopping/src/pages/mainmodule/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './cssmodule/Product.css';
+
+
+function Footer ()
+{
+  
+  
+      return (
+        <div className="Footer">
+       <p>Copyright &copy; 2022,All rights reserved </p>
+      </div>
+    )
+  
+}
+export default Footer;
