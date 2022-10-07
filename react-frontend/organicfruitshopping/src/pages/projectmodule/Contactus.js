e4c4ab58cd8f46d25c443c8eec4cb61433c86eb1
Hari Neeraja Siripalli <neeraja.siripalli@ivycomptech.com>
2022-10-07 11:22:54 +0000

    After adding contactus page

---
+++ react-frontend/organicfruitshopping/src/pages/projectmodule/Contactus.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Headerone from './mainmodule/Headerone';
+
+
+function Contactus() {
+   
+return (
+<div>
+
+<Headerone /><div><a href="/Home" className="backButton" style={{position:'relative',bottom:'2rem'}}> &larr; BACK </a></div>
+<h3>For any queries please contact </h3>
+<p>Contact:8456932277</p>
+<p>email:service.organic@gmail.com</p>
+<div className="Footer" style={{top:'27rem'}} ><p>Copyright &copy; 2022,All rights reserved </p></div>
+</div>
+
+)
+}
+
+export default Contactus;
