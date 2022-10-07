d1b4d556b0ca0609b900e91d7170dfdc0c5e6270
Hari Neeraja Siripalli <neeraja.siripalli@ivycomptech.com>
2022-10-07 11:41:50 +0000

    Initial commit

---
+++ spring-backend/src/main/java/com/organicshopping/fruitshopping/FruitshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.organicshopping.fruitshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitshoppingApplication.class, args);
+	}
+
+}
