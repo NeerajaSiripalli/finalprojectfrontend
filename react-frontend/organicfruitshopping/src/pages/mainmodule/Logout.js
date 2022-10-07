747e3d637b53a95daa74be62204296addd59a6a0
Hari Neeraja Siripalli <neeraja.siripalli@ivycomptech.com>
2022-10-07 11:19:30 +0000

    After implementing logout module

---
+++ react-frontend/organicfruitshopping/src/pages/mainmodule/Logout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function Logout() {
+    const handleSubmit = ()=>{
+        
+    }
+
+return (
+<div>
+
+<button onClick={()=>handleSubmit()} type="submit" class="btn">Logout</button>
+
+</div>
+)
+}
+
+export default Logout;
