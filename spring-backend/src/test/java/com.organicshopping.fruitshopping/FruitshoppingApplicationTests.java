08f8ffa6e983659028bd504077038292f9d9e156
Hari Neeraja Siripalli <neeraja.siripalli@ivycomptech.com>
2022-10-07 11:34:51 +0000

    Initial commit

---
+++ spring-backend/src/test/java/com.organicshopping.fruitshopping/FruitshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organicshopping.fruitshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
