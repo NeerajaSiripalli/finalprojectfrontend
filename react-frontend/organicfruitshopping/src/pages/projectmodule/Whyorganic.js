2b11be4a1fac0ae7acf7e314a38096c3e35e5683
Hari Neeraja Siripalli <neeraja.siripalli@ivycomptech.com>
2022-10-07 11:21:25 +0000

    Adding whyorganic page

---
+++ react-frontend/organicfruitshopping/src/pages/projectmodule/Whyorganic.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Headerone from './mainmodule/Headerone';
+
+function Whyorganic() {
+return (
+<div>
+<Headerone /> <div><a href="/Home" className="backButton" style={{position:'relative',bottom:'2rem'}}> &larr; BACK </a></div>
+    <h5>Why Organic</h5>
+<p>Once found only in health fruit stores,
+organic fruit is now a common feature at most grocery stores.
+And that's made a bit of a problem in the produce aisle.</p>
+<p>Some data shows possible health benefits of organic foods when compared with foods grown using the usual (conventional) process. These studies have shown differences in the food.
+But there is limited information to prove how these differences can give potential overall health benefits.</p>
+<p>Not only does organic production help reduce public health risks, mounting evidence shows that food grown organically are rich in nutrients, such as Vitamin C, iron, magnesium, and phosphorus, with less exposure to nitrates and pesticide residues in organically grown fruits, vegetables, and grains when compared to other methods where chemicals are used.</p>
+<h5>
+    Reasons to consume Organic Fruits
+</h5>
+<p>Persistent Pesticides</p>
+<p>Health</p>
+<p>Soil Health</p>
+<p> Nutrition</p>
+<p>Pollinators</p>
+<div className="Footer" style={{top:'23rem'}} ><p>Copyright &copy; 2022,All rights reserved </p></div>
+</div>
+
+)
+}
+
+export default Whyorganic;
